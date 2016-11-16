c8dbbf261d93ff01b907b9525f2ade0519ef9a61
Quentin Vial-Gouteyron <quentin@mail.atm-consulting.fr>
2016-11-16 17:33:30 +0100

    fix init

--- core/modules/modmetre.class.php
+++ core/modules/modmetre.class.php
@@ -56,7 +56,7 @@ class modmetre extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
-		$this->description = "Description of module metre";
+		$this->description = "Module permettant de calculer un prix en fonction de la longueur et la largeur d'un produit";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = '1.0';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
@@ -87,7 +87,7 @@ class modmetre extends DolibarrModules
 		//							'dir' => array('output' => 'othermodulename'),      // To force the default directories names
 		//							'workflow' => array('WORKFLOW_MODULE1_YOURACTIONTYPE_MODULE2'=>array('enabled'=>'! empty($conf->module1->enabled) && ! empty($conf->module2->enabled)', 'picto'=>'yourpicto@metre')) // Set here all workflow context managed by module
 		//                        );
-		$this->module_parts = array();
+		$this->module_parts = array('hooks'=>array('propalcard', 'ordercard', 'invoicecard'),'triggers' => 1);
 
 		// Data directories to create when module is enabled.
 		// Example: this->dirs = array("/metre/temp");
@@ -248,12 +248,16 @@ class modmetre extends DolibarrModules
 	{
 		$sql = array();
 		
+		$result=$this->load_tables('/metre/sql/');
+
 		define('INC_FROM_DOLIBARR',true);
 
 		dol_include_once('/metre/config.php');
 		dol_include_once('/metre/script/create-maj-base.php');
 
-		$result=$this->_load_tables('/metre/sql/');
+		dol_include_once('/core/class/extrafields.class.php');
+
+		
 
 		return $this->_init($sql, $options);
 	}
